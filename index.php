b9ade54774acab25bbd2868e9f218fbc6ee89ab3
Tjardo18 <332688@student.mboutrecht.nl>
2023-02-02 23:30:41 +0100

    Favicon en pizzaicon toegevoegd aan form titel

--- index.php
+++ index.php
@@ -5,13 +5,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="shortcut icon" href="img/favicon.avif" type="image/x-icon">
     <link rel="stylesheet" href="style/style.css">
     <title>Pizza Maken</title>
 </head>
 
 <body>
     <div class="create">
-        <h1>Maak je eigen pizza</h1>
+        <h1>
+            <i class='bx bxs-pizza bx-flip-horizontal'></i>
+            Maak je eigen pizza
+            <i class='bx bxs-pizza'></i>
+        </h1>
         <form action="createPizza.php" method="POST">
 
             <div class="bodemformaat">
