7d32628710a042b2eb7f3e723ac98f945fb5bcc9
Tjardo18 <332688@student.mboutrecht.nl>
2023-02-05 00:29:57 +0100

    Read tabel werkend gemaakt

---
+++ client/read.php
@@ -0,0 +1,72 @@
+<link rel="stylesheet" href="../style/style.css">
+<?php
+
+require('lib/console_log.php');
+require('config/config.php');
+
+$dsn = "mysql:host=$dbHost;dbname=$dbName;charset=$dbCharset";
+
+try {
+    $pdo = new PDO($dsn, $dbUser, $dbPass);
+} catch (PDOException $e) {
+    echo "<h1>Er is iets fout gegaan tijdens het verbinden met de database. Neem contact op met de Database Beheerder.</h1>";
+    console_log($e->getMessage());
+}
+
+$sql = "SELECT id
+              ,bodemFormaat
+              ,saus
+              ,topping
+              ,kruiden
+        FROM pizza
+        ORDER BY id";
+
+$statement = $pdo->prepare($sql);
+$statement->execute();
+
+$result = $statement->fetchAll(PDO::FETCH_OBJ);
+
+$rows = "";
+foreach ($result as $pizza) {
+    $rows .= "<tr>
+                <td>$pizza->id</td>
+                <td>$pizza->bodemFormaat</td>
+                <td>$pizza->saus</td>
+                <td>$pizza->topping</td>
+                <td>$pizza->kruiden</td>
+                <td>
+                    <a href='delete.php?id={$pizza->id}'>
+                        <img src='img/b_drop.png' alt=''>
+                    </a>
+                </td>
+                <td>
+                    <a href='update.php?id={$pizza->id}'>
+                        <img src='img/b_edit.png' alt=''>
+                    </a>
+                </td>
+             </tr>";
+}
+?>
+<div class="card">
+    <table>
+        <th>ID</th>
+        <th>Bodem Formaat</th>
+        <th>Saus</th>
+        <th>Topping</th>
+        <th>Kruiden</th>
+        <th></th>
+        <th></th>
+        <tr>
+            <?php echo $rows; ?>
+        </tr>
+    </table>
+    <br>
+    <div class="buttons">
+        <a href="../index.php">
+            <input type="submit" value="Nieuwe Pizza">
+        </a>
+        <a href="truncate.php">
+            <input type="submit" class="truncate" value="Truncate">
+        </a>
+    </div>
+</div>
